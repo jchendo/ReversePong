0b75dd846763e48f06d45605a0b1715320d8cbd1
Jacob Henderson <jacobhenderson9127@gmail.com>
2024-06-14 13:51:45 -0500

    basic title screen and more(ish) consistent movement/gameplay

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <windows.h>;
 
 class Game {
 
@@ -13,24 +14,29 @@ private:
     void        render();
     void        handlePlayerInput(sf::Keyboard::Key, bool);
     void        collisionDetection();
+    void        resetGameAttributes();
 
 private:
     sf::RenderWindow   mWindow;
     sf::CircleShape    player;
     sf::RectangleShape leftPaddle, rightPaddle;
     sf::Time           TimePerFrame;
-    sf::Text           ScoreText;
+    sf::Text           ScoreText, TitleText, StartText;
     sf::Font           font;
-    bool               isMovingUp, isMovingDown;
+    sf::Mouse          mouse;
+    sf::Clock          gameClock;
+    bool               isMovingUp, isMovingDown, startGame;
     float              xPlayerSpeed, yPlayerSpeed, lPaddleSpeed, rPaddleSpeed;
-    int32_t            score;
+    int32_t            score, speedLimit;
 
 
 };
 
-Game::Game() : mWindow(sf::VideoMode(1280, 720), "Game"), player(), leftPaddle(), rightPaddle(), 
-               TimePerFrame(), isMovingUp(), isMovingDown() 
+Game::Game() : mWindow(sf::VideoMode(1280, 720), "Reverse Pong"), player(), leftPaddle(), rightPaddle(), 
+               TimePerFrame(), isMovingUp(), isMovingDown(), xPlayerSpeed(), yPlayerSpeed(), 
+               startGame(), speedLimit()
 {
+    speedLimit = 500;
     score = 0;
     TimePerFrame = sf::seconds(1.f / 240.f); // FPS
 
@@ -45,22 +51,34 @@ Game::Game() : mWindow(sf::VideoMode(1280, 720), "Game"), player(), leftPaddle()
 
     // Paddle parameters
     leftPaddle.setSize(sf::Vector2f(10.f,100.f));
-    leftPaddle.setPosition(30.f, 360.f);
     leftPaddle.setFillColor(sf::Color::Blue);
+    leftPaddle.setPosition(30.f, 360.f);
     lPaddleSpeed = -50.f;
 
     rightPaddle.setSize(sf::Vector2f(10.f, 100.f));
-    rightPaddle.setPosition(1250.f, 360.f);
     rightPaddle.setFillColor(sf::Color::Blue);
+    rightPaddle.setPosition(1250.f, 360.f);
     rPaddleSpeed = 50.f;
     
     // Text parameters
-    font.loadFromFile("Arial.ttf");
+    font.loadFromFile("SoulDaisy.otf");
     ScoreText.setFont(font);
-    ScoreText.setCharacterSize(30.f);
-    ScoreText.setString("Score: " + std::to_string(score));
+    ScoreText.setCharacterSize(50.f);
+    ScoreText.setString(std::to_string(score));
     ScoreText.setFillColor(sf::Color::White);
-    ScoreText.setPosition(520.f, 30.f);
+    ScoreText.setPosition(620.f, 30.f);
+
+    TitleText.setFont(font);
+    TitleText.setCharacterSize(100.f);
+    TitleText.setString("Reverse Pong");
+    TitleText.setFillColor(sf::Color::White);
+    TitleText.setPosition(300.f, 30.f);
+
+    StartText.setFont(font);
+    StartText.setCharacterSize(50.f);
+    StartText.setString("START");
+    StartText.setFillColor(sf::Color::White);
+    StartText.setPosition(100.f, 350.f);
    
 }
 
@@ -70,6 +88,11 @@ void Game::run() {
     sf::Time timeSinceLastUpdate = sf::Time::Zero;
 
     while (mWindow.isOpen()) {
+
+        while (!startGame) {
+            processEvents(clock);
+            render();
+        }
 
         processEvents(clock);
         // clock.restart() returns the elapsed time since the clock LAST restarted.
@@ -83,7 +106,7 @@ void Game::run() {
 
         }
 
-        ScoreText.setString("Score: " + std::to_string(score));
+        ScoreText.setString(std::to_string(score));
         render();
 
     }
@@ -108,6 +131,24 @@ void Game::processEvents(sf::Clock clock) {
             handlePlayerInput(event.key.code, false);
             break;
 
+        case sf::Event::MouseButtonPressed:
+            if    (std::abs(mouse.getPosition(mWindow).x) - 150 <= 50 // If mouse position is within START button & pressed.
+                && std::abs(mouse.getPosition(mWindow).y) - 350 <= 50 
+                && !startGame) {
+
+                StartText.setFillColor(sf::Color{ 0x4e545e });
+            }
+            break;
+
+        case sf::Event::MouseButtonReleased:
+            if (StartText.getFillColor() == sf::Color{ 0x4e545e }) {
+
+                StartText.setFillColor(sf::Color::White);
+                mWindow.draw(StartText);
+                Sleep(500); 
+                startGame = true;
+                resetGameAttributes();
+            }
         }
     }
 }
@@ -120,15 +161,16 @@ void Game::collisionDetection() {
 
     // Player - Side Wall collisions.
     // Game over!
-    if (playerLocation.getPosition().x > 1270 || playerLocation.getPosition().x <= 10) {
-        mWindow.close();
+    if ((playerLocation.getPosition().x > 1270 || playerLocation.getPosition().x <= 10) && startGame) {
+        startGame = false;
+        resetGameAttributes();
         std::cout << "Game over!\n";
     }
 
     // Player - Top/Bottom Wall collisions.
     // Maybe make a border object so .intersects() can be used?
     if (playerLocation.getPosition().y > 700 || playerLocation.getPosition().y <= 0) {
-        yPlayerSpeed *= -0.8;
+        yPlayerSpeed *= -0.9;
         // Conditional positional change (680 * 1 if at bottom, 680 * 0 if at top).
         player.setPosition(playerLocation.getPosition().x, (playerLocation.getPosition().y > 700) * 680 + 10);
     }
@@ -143,7 +185,7 @@ void Game::collisionDetection() {
             lPaddleSpeed *= -1;
         }
         // Changing player speed every collision.
-        xPlayerSpeed *= -1.1;
+        xPlayerSpeed *= -1.05;
         score += 1;
     }
 
@@ -165,14 +207,20 @@ void Game::collisionDetection() {
 void Game::update(sf::Time deltaTime) {
 
     collisionDetection();
-    float factor = 1.05; // For ease of testing values - will replace w/ constant
-    int speedLimit = 500;
+    float factor = 1.07; // For ease of testing values - will replace w/ constant
     sf::Vector2f playerMovement(xPlayerSpeed, yPlayerSpeed);
+
+    // The longer the game goes, the faster you can go.
+    if (gameClock.getElapsedTime().asMilliseconds() % 2000 <= 3 && 
+        gameClock.getElapsedTime().asMilliseconds() > 2000) {
+
+        speedLimit += 25;
+    }
 
     if (isMovingUp) {
         // If switching directions, just dampen current speed.
         // Avoids rapid directional changes.
-        if (yPlayerSpeed > 2) {
+        if (yPlayerSpeed > 10) {
             playerMovement.y -= yPlayerSpeed * 0.2;
         }
         else if (yPlayerSpeed > -speedLimit) { // Ensuring always negative (Up is towards 0, i.e. negative speed).
@@ -187,7 +235,7 @@ void Game::update(sf::Time deltaTime) {
     }
     if (isMovingDown) {
 
-        if (yPlayerSpeed < -2) {
+        if (yPlayerSpeed < -10) {
             playerMovement.y -= yPlayerSpeed * 0.2;
         }
         else if (yPlayerSpeed < speedLimit) {
@@ -201,11 +249,11 @@ void Game::update(sf::Time deltaTime) {
   
     }
 
-    // Decay
+    // Speed Decay
     if (yPlayerSpeed >= 1 or yPlayerSpeed < -1) {
-        yPlayerSpeed /= 1.004;
+        yPlayerSpeed /= 1.01;
     }
-
+    
     leftPaddle.move(0.f, lPaddleSpeed * deltaTime.asSeconds());
     rightPaddle.move(0.f, rPaddleSpeed * deltaTime.asSeconds());
     player.move(playerMovement * deltaTime.asSeconds());
@@ -213,12 +261,19 @@ void Game::update(sf::Time deltaTime) {
 }
 
 void Game::render() {
-
-    mWindow.clear(sf::Color::Black);
-    mWindow.draw(player);
-    mWindow.draw(leftPaddle);
-    mWindow.draw(rightPaddle);
-    mWindow.draw(ScoreText);
+    
+    if (startGame) {
+        mWindow.clear(sf::Color::Black);
+        mWindow.draw(player);
+        mWindow.draw(leftPaddle);
+        mWindow.draw(rightPaddle);
+        mWindow.draw(ScoreText);
+    }
+    else {
+        mWindow.clear(sf::Color::Magenta);
+        mWindow.draw(TitleText);
+        mWindow.draw(StartText);
+    }
     mWindow.display();
 
 }
@@ -236,8 +291,25 @@ void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
     }
 }
 
-int main() {
+void Game::resetGameAttributes() {
+    // For starting/restarting game
+    player.setPosition(45.f, 360.f);
+    xPlayerSpeed = 300.f;
+    yPlayerSpeed = 0.5f;
 
+    leftPaddle.setPosition(30.f, 360.f);
+    lPaddleSpeed = -50.f;
+
+    rightPaddle.setPosition(1250.f, 360.f);
+    rPaddleSpeed = 50.f;
+
+    speedLimit = 500;
+    score = 0;
+    gameClock.restart();
+}
+
+int main() {
+   
     Game game;
     // We're able to use the run() function as it exists in the public scope of the Game class.
     game.run();
